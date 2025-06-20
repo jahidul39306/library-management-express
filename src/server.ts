91ddb3baaa5b3e4c01ee5e8d9ecff478693f517f
jahidul39306 <jahidulislamananto@gmail.com>
2025-06-20 12:45:43 +0600

    connected to mongodb

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,28 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose';
+
+
 const app = express()
 dotenv.config()
 const port = process.env.PORT
+const db_uri = process.env.DATABASE_URI!
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+async function main() {
+    try {
+        await mongoose.connect(db_uri);
+        console.log('connected to mongodb')
+        app.listen(port, () => {
+            console.log(`Example app listening on port ${port}`)
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+main()
+
